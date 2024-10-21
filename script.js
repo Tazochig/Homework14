d21a464b3896a27c2cb2bcc633df85930aa0b035
Tazochig <chigladzetazo@gmail.com>
2024-10-21 19:22:42 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let number = 12; 
+let square = number * number;
+
+if (square > 100) {
+  console.log("მეტია ასზე");
+} else {
+  console.log("ნაკლებია ასზე");
+}
+
+let cifri = 62; 
+
+if (50 < cifri && cifri < 100) {
+  console.log("არის");
+} else {
+  console.log("არ არის");
+}
+
+let ricxvi = -62; 
+
+if ( ricxvi <= 0 ) {
+  console.log("არის");
+} else {
+  console.log("არ არის");
+}
+
+let length = 25; 
+let width = 7;
+let P = (2 * (length + width))
+if ( P > 50 ) {
+    console.log("პერიმეტრი არის 50-ზე მეტი");
+  } else {
+    console.log("პერიმეტრი არ არის 50-ზე მეტი");
+  }
+  console.log("პერიმეტრი",P)
